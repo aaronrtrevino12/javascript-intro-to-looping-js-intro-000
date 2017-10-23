ab1df1defa6f40d23cc447f8c0f8239412217dee
Aaron R. Trevino <aaronrtrevino12@gmail.com>
2017-10-23 13:19:16 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -11,7 +11,7 @@ function forLoop(array){
 
 function whileLoop(num){
   while (num > 0) {
-    num -= 1  // subtract one from number
+    num -= 1  // subtract one from number ("original argument")
     console.log(num)
   }
     return 'done'
